c27a1fc5e0c0b4495caa1e163fe64a780742ef95
roife <roifewu@gmail.com>
2024-10-31 00:08:05 +0800

    feat(syntax): helper fn for paired token

---
+++ crates/syntax/src/slang_ext/token.rs
@@ -0,0 +1,28 @@
+use either::Either;
+use slang::{TokenKind, T};
+
+/// [`Either::Left`] represents the beg-token, and [`Either::Right`] represents the
+/// end-token.
+pub fn token_pair(kind: TokenKind) -> Option<Either<TokenKind, TokenKind>> {
+    macro_rules! P {
+        ($beg:ident, $end:ident; $($rest:tt)*) => {
+            if kind == T![$beg] {
+                Some(Either::Right(T![$end]))
+            } else if kind == T![$end] {
+                Some(Either::Left(T![$beg]))
+            } else {
+                P! { $($rest)* }
+            }
+        };
+        () => { None };
+    }
+    P! {
+        begin, end;
+        case, endcase;
+        function, endfunction;
+        generate, endgenerate;
+        interface, endinterface;
+        module, endmodule;
+        task, endtask;
+    }
+}
