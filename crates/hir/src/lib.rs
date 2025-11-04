41705ad350effd8bcfcd25fed5d13995348a172c
hongjr03 <me@jrhim.com>
2025-11-04 17:26:34 +0800

    test(ide): improve test framework to show all failures

--- crates/hir/src/lib.rs
+++ crates/hir/src/lib.rs
@@ -13,3 +13,12 @@ pub mod region_tree;
 pub mod scope;
 pub mod semantics;
 pub mod source_map;
+
+pub use completion::{
+    CompletionEntry, CompletionEntryKind, CompletionScope, DotField, DotFieldKind,
+    ScopedCompletionEntry,
+};
+pub use scope::{
+    AnsiPortEntry, BlockEntry, BlockScope, ModuleEntry, ModuleScope, NonAnsiPortEntry, UnitEntry,
+    UnitScope,
+};
