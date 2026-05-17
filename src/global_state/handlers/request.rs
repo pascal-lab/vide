f9da5ef11a49f9bb4d68e406a3dba191338286f6
hongjr03 <hongjr03@gmail.com>
2026-05-17 16:49:54 +0800

    feat: add vuff formatter provider

--- src/global_state/handlers/request.rs
+++ src/global_state/handlers/request.rs
@@ -372,7 +372,7 @@ pub(crate) fn handle_formatting(
     let file_id = from_proto::file_id(&snap, &params.text_document.uri)?;
     let line_info = snap.line_info(file_id)?;
 
-    let config = snap.config.fmt();
+    let config = formatting_config(&snap, &params.options);
     let edit =
         snap.analysis.format(file_id, None, &line_info, config)?.map_err(to_proto::format_error)?;
 
@@ -389,7 +389,7 @@ pub(crate) fn handle_range_formatting(
     let line_ranges =
         Some((params.range.start.line as usize)..((params.range.end.line as usize) + 1));
 
-    let config = snap.config.fmt();
+    let config = formatting_config(&snap, &params.options);
     let edit = snap
         .analysis
         .format(file_id, line_ranges, &line_info, config)?
@@ -406,7 +406,7 @@ pub(crate) fn handle_on_type_formatting(
     let position = from_proto::file_position(&snap, params.text_document_position)?;
     let line_info = snap.line_info(position.file_id)?;
 
-    let config = snap.config.fmt();
+    let config = formatting_config(&snap, &params.options);
     let edit = snap
         .analysis
         .format_on_type(position, params.ch, &line_info, config)?
@@ -414,6 +414,15 @@ pub(crate) fn handle_on_type_formatting(
 
     let text_edits = edit.map(|edit| to_proto::text_edits(&line_info, edit));
     Ok(text_edits)
+}
+
+fn formatting_config(
+    snap: &GlobalStateSnapshot,
+    options: &lsp_types::FormattingOptions,
+) -> ide::formatting::FmtConfig {
+    let mut config = snap.config.fmt();
+    config.apply_editor_options(options.tab_size, options.insert_spaces);
+    config
 }
 
 pub(crate) fn handle_selection_range(
