cae1b7aa5e48872658b22b4a38265a362fd6bc52
hongjr03 <hongjr03@gmail.com>
2026-05-20 17:13:34 +0800

    refactor(config): parse manifest lsp context

--- crates/project-model/src/lib.rs
+++ crates/project-model/src/lib.rs
@@ -11,7 +11,10 @@ use base_db::{
 };
 use itertools::Itertools;
 use rustc_hash::{FxHashMap, FxHashSet};
-pub use toml_workspace::{TomlManifestDiagnostic, toml_manifest_diagnostics};
+pub use toml_workspace::{
+    TomlManifestDiagnostic, TomlManifestField, TomlManifestPath, toml_manifest_diagnostics,
+    toml_manifest_field_at_offset, toml_manifest_path_at_offset,
+};
 use triomphe::Arc;
 use utils::paths::{AbsPathBuf, sort_and_remove_subfolders};
 use vfs::{FileSetConfig, VfsPath};
