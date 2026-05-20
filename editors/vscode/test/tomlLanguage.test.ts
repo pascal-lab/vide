e581d7b345b4cfbaeaee5f8ead5fcc498fb49b34
hongjr03 <hongjr03@gmail.com>
2026-05-20 18:55:07 +0800

    feat(config): pair toml brackets and quotes

---
+++ editors/vscode/test/tomlLanguage.test.ts
@@ -0,0 +1,13 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+import { TOML_LANGUAGE_CONFIGURATION, TOML_LANGUAGE_ID } from '../src/tomlLanguage';
+
+test('registers TOML bracket and quote pairs for project configs', () => {
+  assert.equal(TOML_LANGUAGE_ID, 'toml');
+  assert.deepEqual(TOML_LANGUAGE_CONFIGURATION.brackets, [['[', ']']]);
+  assert.deepEqual(TOML_LANGUAGE_CONFIGURATION.autoClosingPairs, [
+    { open: '[', close: ']' },
+    { open: '"', close: '"', notIn: [2, 1] },
+  ]);
+});
