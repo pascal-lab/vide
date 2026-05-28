2edb955c710585eae93103d759bb55995cdfc54d
hongjr03 <hongjr03@gmail.com>
2026-05-28 19:36:47 +0800

    chore: allow to skip changelog check

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -131,15 +131,7 @@ export default defineConfig({
         {
           label: 'Changelog',
           translations: { en: 'Changelog' },
-          items: [
-            'changelog',
-            'changelog/v0-1-6',
-            'changelog/v0-1-5',
-            'changelog/v0-1-4',
-            'changelog/v0-1-3',
-            'changelog/v0-1-2',
-            'changelog/v0-1-1',
-          ],
+          items: ['changelog'],
         },
         {
           label: 'Playground',
