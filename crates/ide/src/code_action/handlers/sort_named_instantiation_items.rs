e6c550efc843f087829ee892f4a31f4beee718aa
roife <roifewu@gmail.com>
2026-06-08 11:11:36 +0800

    refactor: prefer using hir

--- crates/ide/src/code_action/handlers/sort_named_instantiation_items.rs
+++ crates/ide/src/code_action/handlers/sort_named_instantiation_items.rs
@@ -1,21 +1,29 @@
 use std::ops::Range;
 
-use hir::{base_db::source_db::SourceDb, db::HirDb};
+use hir::{
+    base_db::source_db::SourceDb,
+    container::InModule,
+    db::HirDb,
+    hir_def::module::instantiation::{ParamAssign, PortConn},
+    source_map::IsSrc,
+};
 use itertools::Itertools;
 use rustc_hash::FxHashMap;
-use smol_str::ToSmolStr;
 use syntax::{
     ast::{self, AstNode},
-    has_text_range::{HasTextRange, HasTextRangeIn},
+    has_text_range::HasTextRangeIn,
 };
-use utils::text_edit::TextRange;
+use utils::{
+    get::{Get, GetRef},
+    text_edit::TextRange,
+};
 
 use crate::{
     code_action::{
         CodeActionCollector, CodeActionCtx, CodeActionId, CodeActionKind, all_parameter_names,
         line_indent, port_names,
     },
-    module_resolution::resolve_instantiation_target,
+    module_resolution::resolve_hir_instantiation_target,
 };
 
 const SORT_NAMED_PARAMETER_ASSIGNMENTS_ID: CodeActionId = CodeActionId {
@@ -41,25 +49,30 @@ pub(super) fn sort_named_parameter_assignments(
     collector: &mut CodeActionCollector,
     ctx: &CodeActionCtx,
 ) -> Option<()> {
-    let instantiation = ctx.find_node_at_offset::<ast::HierarchyInstantiation>()?;
-    let params = instantiation.parameters()?;
+    let ast_instantiation = ctx.find_node_at_offset::<ast::HierarchyInstantiation>()?;
+    let params = ast_instantiation.parameters()?;
     let open = params.open_paren()?.text_range_in(params.syntax())?;
     let close = params.close_paren()?.text_range_in(params.syntax())?;
 
-    let db = ctx.sema().db;
-    let target_module_id =
-        resolve_instantiation_target(db, ctx.file_id(), instantiation).unique()?;
+    let sema = ctx.sema();
+    let db = sema.db;
+    let InModule { value: instantiation_id, module_id } =
+        sema.resolve_instantiation(ctx.file_id().into(), ast_instantiation)?;
+    let (module, module_src_map) = db.module_with_source_map(module_id);
+    let instantiation = module.get(instantiation_id);
+    let target_module_id = resolve_hir_instantiation_target(db, ctx.file_id(), instantiation)?;
     let parameter_order = all_parameter_names(&db.module(target_module_id));
     let parameter_order_map: FxHashMap<_, _> =
         parameter_order.iter().enumerate().map(|(index, name)| (name.as_ref(), index)).collect();
 
-    let text = ctx.sema().db.file_text(ctx.file_id());
+    let text = sema.db.file_text(ctx.file_id());
     let mut items = Vec::new();
-    for assign in params.parameters().children() {
-        let named = assign.as_named_param_assignment()?;
-        let name = named.name()?.value_text().to_smolstr();
+    for assign_id in instantiation.param_assigns.iter() {
+        let ParamAssign::Named(Some(name), _) = module.get(*assign_id) else {
+            return None;
+        };
         let order = *parameter_order_map.get(name.as_str())?;
-        let range = assign.syntax().text_range()?;
+        let range = module_src_map.get(*assign_id)?.range();
         items.push((order, text.get(Range::from(range))?, range));
     }
 
@@ -97,26 +110,30 @@ pub(super) fn sort_named_port_connections(
     collector: &mut CodeActionCollector,
     ctx: &CodeActionCtx,
 ) -> Option<()> {
-    let instance = ctx.find_node_at_offset::<ast::HierarchicalInstance>()?;
-    let instantiation = ast::HierarchyInstantiation::cast(instance.syntax().parent()?)?;
-    let open = instance.open_paren()?.text_range_in(instance.syntax())?;
-    let close = instance.close_paren()?.text_range_in(instance.syntax())?;
+    let ast_instance = ctx.find_node_at_offset::<ast::HierarchicalInstance>()?;
+    let open = ast_instance.open_paren()?.text_range_in(ast_instance.syntax())?;
+    let close = ast_instance.close_paren()?.text_range_in(ast_instance.syntax())?;
 
-    let db = ctx.sema().db;
-    let target_module_id =
-        resolve_instantiation_target(db, ctx.file_id(), instantiation).unique()?;
-    let target_module = db.module(target_module_id);
-    let port_order = port_names(&target_module);
+    let sema = ctx.sema();
+    let db = sema.db;
+    let InModule { value: instance_id, module_id } =
+        sema.resolve_instance(ctx.file_id().into(), ast_instance)?;
+    let (module, module_src_map) = db.module_with_source_map(module_id);
+    let instance = module.get(instance_id);
+    let instantiation = module.get(instance.parent);
+    let target_module_id = resolve_hir_instantiation_target(db, ctx.file_id(), instantiation)?;
+    let port_order = port_names(&db.module(target_module_id));
     let port_order_map: FxHashMap<_, _> =
         port_order.iter().enumerate().map(|(index, name)| (name.as_ref(), index)).collect();
 
-    let text = ctx.sema().db.file_text(ctx.file_id());
+    let text = sema.db.file_text(ctx.file_id());
     let mut items = Vec::new();
-    for conn in instance.connections().children() {
-        let named = conn.as_named_port_connection()?;
-        let name = named.name()?.value_text().to_smolstr();
+    for conn_id in instance.connections.iter() {
+        let PortConn::Named(Some(name), _) = module.get(*conn_id) else {
+            return None;
+        };
         let order = *port_order_map.get(name.as_str())?;
-        let range = conn.syntax().text_range()?;
+        let range = module_src_map.get(*conn_id)?.range();
         items.push((order, text.get(Range::from(range))?, range));
     }
 
