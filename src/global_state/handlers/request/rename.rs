4a2bed5bba54bfc7d61f8da2b194f5109f0d52e9
hongjr03 <hongjr03@gmail.com>
2026-06-16 22:10:22 +0800

    refactor: split request handlers by protocol domain

---
+++ src/global_state/handlers/request/rename.rs
@@ -0,0 +1,35 @@
+﻿use crate::{
+    global_state::snapshot::GlobalStateSnapshot,
+    lsp_ext::{from_proto, to_proto},
+};
+
+pub(crate) fn handle_prepare_rename(
+    snap: GlobalStateSnapshot,
+    params: lsp_types::TextDocumentPositionParams,
+) -> anyhow::Result<Option<lsp_types::PrepareRenameResponse>> {
+    let position = from_proto::file_position(&snap, params)?;
+    let config = snap.rename_config(position.file_id);
+    let line_index = snap.line_info(position.file_id)?;
+
+    let text_range = snap
+        .analysis
+        .prepare_rename(position, config)?
+        .map_err(|err| to_proto::rename_error(snap.config.i18n, err))?;
+    let range = to_proto::range(&line_index, text_range);
+    Ok(Some(lsp_types::PrepareRenameResponse::Range(range)))
+}
+
+pub(crate) fn handle_rename(
+    snap: GlobalStateSnapshot,
+    params: lsp_types::RenameParams,
+) -> anyhow::Result<Option<lsp_types::WorkspaceEdit>> {
+    let position = from_proto::file_position(&snap, params.text_document_position)?;
+    let config = snap.rename_config(position.file_id);
+    let change = snap
+        .analysis
+        .rename(position, config, &params.new_name)?
+        .map_err(|err| to_proto::rename_error(snap.config.i18n, err))?;
+
+    let workspace_edit = to_proto::workspace_edit(&snap, change)?;
+    Ok(Some(workspace_edit))
+}
