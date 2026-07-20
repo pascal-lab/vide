a48aae558da3300f037c4c1a724bfdd85f7c45f4
kunlinglio <142668432+kunlinglio@users.noreply.github.com>
2026-07-20 18:17:19 +0800

    feat(slang-sys): improve clangd support

--- crates/slang-sys/src/lib.rs
+++ crates/slang-sys/src/lib.rs
@@ -8,7 +8,17 @@ pub fn parse_root_kind(text: &str) -> u16 {
 mod tests {
     #[test]
     fn rust_calls_upstream_slang_parser() {
-        let root_kind = crate::parse_root_kind("module demo; endmodule");
+        let test_verilog_code = r#"
+module demo(
+    input wire a,
+    output wire b
+);
+begin
+    assign b = a;
+end
+endmodule
+        "#;
+        let root_kind = crate::parse_root_kind(test_verilog_code);
 
         assert_ne!(root_kind, 0);
     }
