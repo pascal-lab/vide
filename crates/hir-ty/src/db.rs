d0010a73ddff0f68d422dcad77d175424e8688d8
Hong Jiarong <me@jrhim.com>
2026-07-25 16:30:21 +0800

    refactor(hir-ty): make type handles cheap to clone

--- crates/hir-ty/src/db.rs
+++ crates/hir-ty/src/db.rs
@@ -1,15 +1,14 @@
 use hir_def::{
     container::InContainer, db::HirDefDb, def_id::DefId, expr::ExprId, symbol::Resolution,
 };
-use triomphe::Arc;
 
 use crate::Type;
 
 #[salsa::query_group(TyDbStorage)]
 pub trait TyDb: HirDefDb {
     #[salsa::invoke(crate::infer::type_of_expr_query)]
-    fn infer_expr(&self, expr: InContainer<ExprId>) -> Arc<Type>;
+    fn infer_expr(&self, expr: InContainer<ExprId>) -> Type;
 
     #[salsa::invoke(crate::infer::type_of_path_resolution_query)]
-    fn infer_path_resolution(&self, res: Resolution<DefId>) -> Arc<Type>;
+    fn infer_path_resolution(&self, res: Resolution<DefId>) -> Type;
 }
